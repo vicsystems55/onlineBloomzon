a5424b8c3694cb0a3c8083eca2ffb803b42931c2
vicSystesms <46149752+vicsystems55@users.noreply.github.com>
2020-06-10 20:49:39 +0100

    further adjustment

--- app/Http/Controllers/SocialiteController.php
+++ app/Http/Controllers/SocialiteController.php
@@ -31,9 +31,19 @@ function createUser($getInfo,$provider){
  $user = User::where('provider_id', $getInfo->id)->first();
  
  if (!$user) {
+     $parts = explode("@", $getInfo->email);
+        $username = $parts[0].rand(11,99);
      $user = User::create([
         'name'     => $getInfo->name,
         'email'    => $getInfo->email,
+        'buyersId' => $username,
+        'avatar' => $getInfo->avatar,
+        'phone' => '08000000000',
+        'role' => 'buyer',
+        'address' => 'Please Update',
+        'gender' => '',
+        'pin'   => '',
+        'password' => 'from google',
         'provider' => $provider,
         'provider_id' => $getInfo->id
     ]);
