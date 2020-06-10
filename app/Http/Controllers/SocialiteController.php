314cf1b5dd8f4f09c7963576a3618bd65922a7ca
vicSystesms <46149752+vicsystems55@users.noreply.github.com>
2020-06-10 15:16:33 +0100

    solution to socialite controller

--- app/Http/Controllers/SocialiteController.php
+++ app/Http/Controllers/SocialiteController.php
@@ -1,60 +1,43 @@
 <?php
-  
-namespace App\Http\Controllers\Auth;
-  
-use App\Http\Controllers\Controller;
+ 
+namespace App\Http\Controllers;
+ 
+use Illuminate\Http\Request;
+use Validator,Redirect,Response,File;
 use Socialite;
-use Auth;
-use Exception;
 use App\User;
-  
+
 class SocialiteController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function redirectToGoogle()
-    {
-        return Socialite::driver('google')->redirect();
-    }
-      
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function handleGoogleCallback()
-    {
-        try {
-    
-            $user = Socialite::driver('google')->user();
+public function redirectToGoogle()
+{
+    return Socialite::driver('google')->redirect();
+}
+ 
+public function callback()
+{
+           
+    $getInfo = Socialite::driver('google')->user();
      
-            $finduser = User::where('provider_id', $user->id)->first();
-     
-            if($finduser){
-     
-                Auth::login($finduser);
-    
-                return redirect('/dashboard');
-     
-            }else{
-                $newUser = User::create([
-                    'name' => $user->name,
-                    'email' => $user->email,
-                    'provider' => 'google',
-                    'provider_id'=> $user->id,
-                    
-                ]);
-    
-                Auth::login($newUser);
-     
-                return redirect('/dashboard');
-            }
-    
-        } catch (Exception $e) {
-            dd($e->getMessage());
-        }
-    }
+    $user = $this->createUser($getInfo,'google');
+ 
+    auth()->login($user);
+ 
+    return redirect()->to('/home');
+ 
+}
+function createUser($getInfo,$provider){
+ 
+ $user = User::where('provider_id', $getInfo->id)->first();
+ 
+ if (!$user) {
+     $user = User::create([
+        'name'     => $getInfo->name,
+        'email'    => $getInfo->email,
+        'provider' => $provider,
+        'provider_id' => $getInfo->id
+    ]);
+  }
+  return $user;
+}
 }
